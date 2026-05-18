eb032f9b68b7a0bb63d7abf2bdbeddfb1d6dd7b6
Retaj <Retaj18999@gmail.com>
2026-05-19 00:51:47 +0300

    deploy web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -51,7 +51,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d7883076e5f546e97e1adcb4f31e89c1
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "3c3fcf377cb1ec40468ada2d2a3839b2",
+"flutter_bootstrap.js": "e8b82bd8bae09dc336e87422f037b0d2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
